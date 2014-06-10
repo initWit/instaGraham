339d4644f5b250cfc6b557db155fb04a6e4facfd
Dennis Dixon <ddixonjr@appivot.com>
2014-06-10 14:37:49 -0500

    Added test protocol and API methods for custom delegation and completion block

--- InstaGraham/InstaGrahamModelManager.h
+++ InstaGraham/InstaGrahamModelManager.h
@@ -8,8 +8,21 @@
 
 #import <Parse/Parse.h>
 
+@class InstaGrahamModelManager;
+
+@protocol InstaGrahamModelManagerDelegate
+
+- (void)modelManager:(InstaGrahamModelManager *)modelManager didPullPhotoSet:(NSArray *)photoSet;
+
+@end
+
 @interface InstaGrahamModelManager : PFObject
 
+@property (strong, nonatomic) id<InstaGrahamModelManagerDelegate> delegate;
+
+- (void)getPhotoSetOnUser:(PFUser *)username includingFollowings:(BOOL)includingFollowings completion:(void (^)(NSArray *photoSet))completion;
+
+- (void)getPhotoSetOnUser:(PFUser *)username includingFollowings:(BOOL)includingFollowings;
 
 
 @end
--- InstaGraham/InstaGrahamModelManager.m
+++ InstaGraham/InstaGrahamModelManager.m
@@ -10,5 +10,20 @@
 
 @implementation InstaGrahamModelManager
 
+@synthesize delegate = _delegate;
+
+- (void)getPhotoSetOnUser:(PFUser *)username includingFollowings:(BOOL)includingFollowings completion:(void (^)(NSArray *photoSet))completion;
+{
+    if (completion)
+    {
+        completion(@[@"this",@"is",@"a",@"test"]);
+    }
+}
+
+
+- (void)getPhotoSetOnUser:(PFUser *)username includingFollowings:(BOOL)includingFollowings;
+{
+    [self.delegate modelManager:self didPullPhotoSet:@[@"this",@"is",@"a",@"test"]];
+}
 
 @end
