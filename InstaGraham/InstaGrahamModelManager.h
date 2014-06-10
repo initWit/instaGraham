47a4d334f7ff29193429aac6fa692c69aaad0749
Dennis Dixon <ddixonjr@appivot.com>
2014-06-10 13:06:25 -0500

    Added empty model class file

---
+++ InstaGraham/InstaGrahamModelManager.h
@@ -0,0 +1,15 @@
+//
+//  InstaGrahamModelManager.h
+//  InstaGraham
+//
+//  Created by Dennis Dixon on 6/10/14.
+//
+//
+
+#import <Parse/Parse.h>
+
+@interface InstaGrahamModelManager : PFObject
+
+
+
+@end
---
+++ InstaGraham/InstaGrahamModelManager.m
@@ -0,0 +1,14 @@
+//
+//  InstaGrahamModelManager.m
+//  InstaGraham
+//
+//  Created by Dennis Dixon on 6/10/14.
+//
+//
+
+#import "InstaGrahamModelManager.h"
+
+@implementation InstaGrahamModelManager
+
+
+@end
