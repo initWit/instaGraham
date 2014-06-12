853f7ca7a788678206fe3908a9aad97ada6da501
initWit <figueras.robert@gmail.com>
2014-06-12 11:10:54 -0500

    posting photo and comment working

---
+++ InstaGraham/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  InstaGraham
+//
+//  Created by Robert Figueras on 6/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
